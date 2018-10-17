ff773a01b8de9f28be9fb83831275f5711731eb4
Olexiy Sokurenko <ol.sakura@gmail.com>
2018-10-17 21:12:23 +0200

    Initial project add

---
+++ src/main/java/de/olexiy/spring/mypetclinic/MyPetClinicApplication.java
@@ -0,0 +1,12 @@
+package de.olexiy.spring.mypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetClinicApplication.class, args);
+    }
+}
