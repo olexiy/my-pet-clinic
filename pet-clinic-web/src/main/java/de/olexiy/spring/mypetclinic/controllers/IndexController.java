cc12ad9d0dfab20682529b8de3223e90dbae848f
Olexiy Sokurenko <ol.sakura@gmail.com>
2018-10-19 18:23:11 +0200

    Index page controller and page created. Closes #9.

---
+++ pet-clinic-web/src/main/java/de/olexiy/spring/mypetclinic/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package de.olexiy.spring.mypetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Olexiy Sokurenko on Okt, 2018
+ **/
+@Controller
+public class IndexController {
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
