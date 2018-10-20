6a6e8d48f4d3cc69f6c76866cffbcec7000f8be4
Olexiy Sokurenko <ol.sakura@gmail.com>
2018-10-21 00:52:07 +0200

    Addad Visit Entity, added Pet to Owner. Closes #22.

--- pet-clinic-data/src/main/java/de/olexiy/spring/mypetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/de/olexiy/spring/mypetclinic/model/Owner.java
@@ -1,8 +1,19 @@
 package de.olexiy.spring.mypetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Olexiy Sokurenko on Okt, 2018
  **/
 public class Owner extends Person {
 
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/de/olexiy/spring/mypetclinic/model/Visit.java
@@ -0,0 +1,37 @@
+package de.olexiy.spring.mypetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * Created by olsak on Okt, 2018
+ **/
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
