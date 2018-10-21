f1017a724a25106d12da248424a8acb3a38a47e6
Olexiy Sokurenko <ol.sakura@gmail.com>
2018-10-21 09:04:43 +0200

    Pet Type Map Service added. Closes #24.

---
+++ pet-clinic-data/src/main/java/de/olexiy/spring/mypetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package de.olexiy.spring.mypetclinic.services;
+
+import de.olexiy.spring.mypetclinic.model.PetType;
+
+/**
+ * Created by olsak on Okt, 2018
+ **/
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/de/olexiy/spring/mypetclinic/services/map/PetTypeServiceMapImp.java
@@ -0,0 +1,36 @@
+package de.olexiy.spring.mypetclinic.services.map;
+
+import de.olexiy.spring.mypetclinic.model.PetType;
+import de.olexiy.spring.mypetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+/**
+ * Created by olsak on Okt, 2018
+ **/
+public class PetTypeServiceMapImp extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public PetType save(PetType obj) {
+        return super.save(obj);
+    }
+
+    @Override
+    public void delete(PetType obj) {
+        super.delete(obj);
+    }
+}
