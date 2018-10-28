f32938dc1a85c6feca5ebdb206a0c6ce90487078
Olexiy Sokurenko <ol.sakura@gmail.com>
2018-10-28 14:42:27 +0100

    Base JPA entity created. Closes #28.

--- pet-clinic-data/src/main/java/de/olexiy/spring/mypetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/de/olexiy/spring/mypetclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package de.olexiy.spring.mypetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by Olexiy Sokurenko on Okt, 2018
  **/
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
